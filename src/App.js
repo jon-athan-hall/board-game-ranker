78f6c17446c0216c5f9483f76cdf4aeef0121183
Jonathan Hall <jonathan.edward.hall@gmail.com>
2021-07-14 18:15:25 -0400

    Style form using Tailwind class names only.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LookupForm from './LookupForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full h-screen p-2 lg:p-16">
       <LookupForm />
     </div>
   );
--- src/LookupForm.js
+++ src/LookupForm.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 class LookupForm extends Component {
   render() {
     return (
-      <form>
-        <input type="text">
+      <form className="flex flex-col p-8 space-y-4">
+        <label for="username">BoardGameGeek username</label>
+        <input type="text" className="p-4 border border-gray-500 rounded-md">
         </input>
-        <button type="submit">
+        <button type="submit" className="p-4 bg-yellow-300 rounded-md">
           Get my played games
         </button>
       </form>
