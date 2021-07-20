9c5fd0ebac04605e63856799ee84fe115e0ddbea
Jonathan Hall <jonathan.edward.hall@gmail.com>
2021-07-19 21:51:39 -0400

    Move fetch into its own method. Use setInterval to continue fetch attempts until 200 success.

--- src/LookupForm.js
+++ src/LookupForm.js
@@ -5,10 +5,12 @@ class LookupForm extends Component {
     super(props);
     this.state = {
       username: 'username',
-      games: []
+      games: [],
+      disabled: false
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.tryFetch = this.tryFetch.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -18,30 +20,49 @@ class LookupForm extends Component {
     });
   }
 
-  handleSubmit(e) {
-    e.preventDefault();
+  tryFetch() {
     const gamesPromise = fetch(`http://www.boardgamegeek.com/xmlapi2/collection?username=${this.state.username}&played=1&excludesubtype=boardgameexpansion&brief=1`);
     gamesPromise
-        .then(response => {
-          if (response.status === 200)
-            return response.text();
-          else
-            console.log(response.status);
-        })
-        .then(text => {
-          console.log(text);
-          const responseDocument = new DOMParser().parseFromString(text, 'application/xml');
+      .then(response => {
+        if (response.status === 200) {
+          clearInterval(this.fetchInterval);
+          return response.text();
+        } else {
+          throw new Error(response.status);
+        }
+      })
+      .then(text => {
+        const responseDocument = new DOMParser().parseFromString(text, 'application/xml');
 
-          /**
-           * Use spread operator to turn NodeList into Array. Then map over the nodes to pull out the game name only.
-           */
-          const games = [...responseDocument.getElementsByTagName('name')].map(game => game.textContent);
+        /**
+         * Use spread operator to turn NodeList into Array. Then map over the nodes to pull out the game name only.
+         */
+        const games = [...responseDocument.getElementsByTagName('name')].map(game => game.textContent);
 
-          console.log(games);
-          this.setState({
-            games
-          });
+        this.setState({
+          disabled: false,
+          games
         });
+
+        console.log(this.state.games);
+      })
+      .catch(status => {
+        console.log(`Response status: ${status}`);
+      });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      disabled: true
+    });
+
+    this.fetchInterval = setInterval(this.tryFetch, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchInterval);
   }
 
   render() {
@@ -55,7 +76,10 @@ class LookupForm extends Component {
           onChange={this.handleChange}
           className="p-4 border border-gray-500 rounded-md"
         />
-        <button type="submit" className="p-4 bg-yellow-300 rounded-md">
+        <button
+          type="submit"
+          disabled={this.state.disabled}
+          className="p-4 bg-yellow-300 rounded-md">
           {this.props.buttonText}
         </button>
       </form>
