a0a79126af48be47c224445075f045dcea108a22
Jonathan Hall <jonathan.edward.hall@gmail.com>
2021-08-02 10:25:18 -0400

    Get bottom-up merge sort outer most loop working.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import LookupForm from './LookupForm';
-import GameButton from './GameButton';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      games: ["7 Wonders", "A Feast for Odin", "Wingspan", "The Castles of Burgundy", "Race for the Galaxy", "Empires of the North", "Clank!: A Deck-Building Adventure"]
+      games: ["4", "7", "6", "1", "3", "2", "5"],
+      finished: false,
+      width: 1,
+      leftIndex: 0,
+      rightIndex: 1
     }
 
     this.setGames = this.setGames.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
   }
 
   setGames(games) {
     this.setState({
       games
+    });
+  }
+
+  handleButtonClick(e) {
+    let finished = this.state.finished;
+    let width = this.state.width;
+    let [leftIndex, rightIndex] = [this.state.leftIndex, this.state.rightIndex];
+    const length = this.state.games.length;
+
+    leftIndex = leftIndex + width * 2;
+    rightIndex = rightIndex + width * 2;
+
+    if (rightIndex >= length) {
+      width = width * 2;
+      leftIndex = 0;
+      rightIndex = Math.min(leftIndex + width, length);
+    }
+
+    if (width >= length) {
+      finished = true;
+    }
+
+    this.setState({
+      finished,
+      width,
+      leftIndex,
+      rightIndex
     });
   }
 
@@ -28,12 +59,25 @@ class App extends Component {
           setGames={this.setGames}
           buttonText="Get my played games"
         />
-        {this.state.games.length > 2 &&
+        {!this.state.finished &&
           <div>
             <h2 className="text-xl font-bold text-center mb-8">Which one is better?</h2>
+            <p>games: {this.state.games}</p>
             <div className="flex space-x-8">
-              <GameButton game={this.state.games[0]} />
-              <GameButton game={this.state.games[1]} />
+              <button
+                className="w-1/2 p-4 bg-yellow-600 rounded-md"
+                value="left"
+                onClick={this.handleButtonClick}
+              >
+                {this.state.games[this.state.leftIndex]}
+              </button>
+              <button
+                className="w-1/2 p-4 bg-yellow-600 rounded-md"
+                value="right"
+                onClick={this.handleButtonClick}
+              >
+                {this.state.games[this.state.rightIndex]}
+              </button>
             </div>
           </div>
         }
